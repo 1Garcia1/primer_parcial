1616dece2d921451c30c69430fe5138be1de11c8
1Garcia1 <garciaf781@gmail.com>
2022-08-08 20:49:46 -0600

    0.2

--- src/App.js
+++ src/App.js
@@ -4,20 +4,53 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className='ti1'>
+        <p className='lead text-center'>¿por qué es importante vacunarse?</p>
+      </div>
+      <div className='text1'>
+        <p className='lead text-start'>
+                                        Vacunarse contra el COVID-19 es una forma más segura y confiable de
+                                        generar protección que contraer el COVID-19. La vacunación contra el COVID-19
+                                        ayuda a protegerlo al crear una respuesta de generación de anticuerpos sin que
+                                        tenga que experimentar una enfermedad posiblemente grave ni afecciones
+                                        posteriores al COVID-19. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className='ti2'>
+        <p className='lead text-center'>¿Cuáles son los beneficios de vacunarse contra el COVID-19? </p>
+      </div>
+      <div className='text2'>
+        <p className='lead text-start'>
+                                        En ese sentido, la vacunación contra el COVID-19, permitirá
+                                        reducir el riesgo de enfermar de forma grave y morir, pues se estará mejor
+                                        protegido. No se alcanzará la inmunidad al 100% ya que igual se puede contraer la enfermedad, sin embargo, 
+                                        las consecuencias en el organismo serán mucho menores. 
+                                        Los principales beneficios son: 
+                                         
+        </p>
+        </div>
+        <div className='text3'>
+        <p className='lead text-start'>                                    
+                                        •Las vacunas contra el COVID-19 también pueden evitar que se enferme 
+                                        gravemente aunque contraiga el virus.. 
+        </p>
+        </div>
+        <div className='text4'>
+        <p className='lead text-start'>                                    
+                                        •Al vacunarse uno mismo también protege a las personas que lo rodean.
+        </p>
+        </div>
+        <div className='text3'>
+        <p className='lead text-start'>                                    
+                                        •Todas las vacunas contra el Covid-19 son seguras y efectivas.
+                                        
+        </p>
+        </div>
+        <div className='text3'>
+        <p className='lead text-start'>                                    
+                                        •Es una forma más segura de desarrollar la inmunidad. 
+        </p>
+        </div>
     </div>
   );
 }
